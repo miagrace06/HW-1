dc56432d4e21fbe1a55ea6485b626c6e2313d3c2
miagrace06 <mcampanella@ucsb.edu>
2025-07-08 11:25:09 -0700

    Update PauseVec.cpp

--- pausevec/PauseVec.cpp
+++ pausevec/PauseVec.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 PauseVec::PauseVec() {
-    count = 0;
-    capacity = 8;
-    min_removed = capacity;
+    count_ = 0;
+    capacity_ = 8;
+    min_removed = capacity_;
     
-    data = new int[capacity];
-    is_removed = new bool[capacity];
-    for (size_t i = 0; i < capacity; i++){
+    data = new int[capacity_];
+    is_removed = new bool[capacity_];
+    for (size_t i = 0; i < capacity_; i++){
         is_removed[i] = false;
     }
 }
@@ -21,18 +21,18 @@ PauseVec::~PauseVec(){
 }
 
 size_t PauseVec::capacity() const {
-    return capacity;
+    return capacity_;
 }
 
 size_t PauseVec::count() const {
-    return count;
+    return count_;
 }
 
 void PauseVec::append(int value){
-    if (count == capacity){
-        int* new_data = new int[capacity * 2];
-        int* new_is_removed = new bool[capacity * 2];
-        for (size_t i = 0; i < count; i++){
+    if (count_ == capacity_){
+        int* new_data = new int[capacity_ * 2];
+        bool* new_is_removed = new bool[capacity_ * 2];
+        for (size_t i = 0; i < count_; i++){
             new_data[i] = data[i];
             new_is_removed[i] = is_removed[i];
         }
@@ -41,57 +41,57 @@ void PauseVec::append(int value){
         
         data = new_data;
         is_removed = new_is_removed;
-        capacity = capacity * 2;
+        capacity_ = capacity_ * 2;
         
     }
-    data[count] = value;
-    is_removed[count] = false; 
-    count++;
+    data[count_] = value;
+    is_removed[count_] = false; 
+    count_++;
 }
 
 int PauseVec::remove(size_t index) {
-    if (index >= count) {
+    if (index >= count_) {
         throw std::out_of_range("Index out of range.");
     }
     
     if (index > min_removed) {
         size_t writeIndex = min_removed;
-        for (size_t i = min_removed; i < count; i++) {
+        for (size_t i = min_removed; i < count_; i++) {
             if (!is_removed[i]) {
                 data[writeIndex] = data[i];
                 is_removed[writeIndex] = false;
                 writeIndex++;
             }
         }
-        count = writeIndex;
-        min_removed = capacity;
+        count_ = writeIndex;
+        min_removed = capacity_;
     }
     
     int removedValue = data[index];
     is_removed[index] = true;
     
-    if (min_removed == capacity || index < min_removed) {
+    if (min_removed == capacity_ || index < min_removed) {
         min_removed = index;
     }
     
     if (index == min_removed) {
         size_t writeIndex = min_removed;
-        for (size_t i = min_removed; i < count; i++) {
+        for (size_t i = min_removed; i < count_; i++) {
             if (!is_removed[i]) {
                 data[writeIndex] = data[i];
                 is_removed[writeIndex] = false;
                 writeIndex++;
             }
         }
-        count = writeIndex;
-        min_removed = capacity;
+        count_ = writeIndex;
+        min_removed = capacity_;
     }
     
-    if (capacity > 8 && count <= capacity / 4) {
-        size_t new_capacity = capacity / 2;
+    if (capacity_ > 8 && count_ <= capacity_ / 4) {
+        size_t new_capacity = capacity_ / 2;
         int* new_data = new int[new_capacity];
         bool* new_is_removed = new bool[new_capacity];
-        for (size_t i = 0; i < count; i++) {
+        for (size_t i = 0; i < count_; i++) {
             new_data[i] = data[i];
             new_is_removed[i] = false;  
         }
@@ -99,28 +99,28 @@ int PauseVec::remove(size_t index) {
         delete[] is_removed;
         data = new_data;
         is_removed = new_is_removed;
-        capacity = new_capacity;
+        capacity_ = new_capacity;
     }
     
     return removedValue;
 }
 
 int PauseVec::lookup(size_t index) {
-    if (index >= count) {
+    if (index >= count_) {
         throw std::out_of_range("Index out of range.");
     }
     
     if (index > min_removed) {
         size_t writeIndex = min_removed;
-        for (size_t i = min_removed; i < count; i++) {
+        for (size_t i = min_removed; i < count_; i++) {
             if (!is_removed[i]) {
                 data[writeIndex] = data[i];
                 is_removed[writeIndex] = false;
                 writeIndex++;
             }
         }
-        count = writeIndex;
-        min_removed = capacity;
+        count_ = writeIndex;
+        min_removed = capacity_;
     }
     
     if (is_removed[index]) {
