b8a586cd2cddffadbf016fa597fff33a3c7ba447
miagrace06 <mcampanella@ucsb.edu>
2025-07-08 11:38:04 -0700

    Update PauseVec.h

--- pausevec/PauseVec.h
+++ pausevec/PauseVec.h
@@ -1,29 +1,28 @@
- #ifndef PAUSEVEC_H
+#ifndef PAUSEVEC_H
 #define PAUSEVEC_H
-
 #include <cstddef>
 
 class PauseVec {
 private:
-int* data;
-bool* is_removed;
-size_t count_;
-size_t capacity_;
-size_t min_removed;
-
+    int* data;
+    bool* is_removed;
+    size_t count_;
+    size_t capacity_;
+    size_t min_removed;
+    
+    void compact();
+    void resize(size_t new_capacity);
+    
 public:
-  PauseVec();
-  ~PauseVec();
-
-  size_t capacity() const;
-  size_t count() const;
-
-  int    lookup(size_t index);
-  int    remove(size_t index);
-  void	 remove_val(int x);
-  void   append(int value);
+    PauseVec();
+    ~PauseVec();
+    size_t capacity() const;
+    size_t count() const;
+    int    lookup(size_t index);
+    int    remove(size_t index);
+    void   remove_val(int x);
+    void   append(int value);
 };
 
 PauseVec* create_pausevec();
-
 #endif
